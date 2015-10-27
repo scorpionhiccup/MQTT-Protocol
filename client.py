cdcbbd14f3bff8185d3bec72525ceeb0f1efe78a
scorpionhiccup <sharvil.2009@gmail.com>
2015-10-27 23:29:15 +0400

    Adding Message Retention

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ def on_connect(client, userdata, rc):
 	print("Connected with result code "+str(rc))
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
-	client.subscribe("sensor-data-topic")
+	client.subscribe("Sensor-data-Topic")
 
 # 
 def on_message(client, userdata, msg):
--- publish.py
+++ publish.py
@@ -3,5 +3,7 @@ import paho.mqtt.client as mqtt
 
 mqttc = mqtt.Client("publisher")
 mqttc.connect("localhost", 1883)
-mqttc.publish("sensor-data-topic", "Sensor Data: Hello, World!")
+# mqttc.publish("Sensor-data-Topic", "Sensor Data: Hello, World!")
+# To enable message retention.
+mqttc.publish("Sensor-data-Topic", "Sensor Data: Hello, World!", 0, True)
 mqttc.loop(2) 
